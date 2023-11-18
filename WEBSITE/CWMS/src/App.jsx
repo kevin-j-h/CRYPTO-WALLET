54b5014256ca16defb835f2990d16d2abdc10c55
Jeswin Binu <jeswinbinu2003@gmail.com>
2023-11-18 19:25:41 +0530

    praise the lord

--- WEBSITE/CWMS/src/App.jsx
+++ WEBSITE/CWMS/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 
@@ -6,6 +7,11 @@ import Login from './pages/login'
 import Website from './pages/website'
 import Signup from './pages/signup'
 import Home from './pages/Home'
+import Watchlist from './pages/Watchlist'
+import Transactions from './pages/Transactions'
+import Wallet from './pages/Wallet'
+import Settings from './pages/Settings'
+
 const App = () => {
   return (
     <Router>
@@ -15,7 +21,12 @@ const App = () => {
         <Route path='/' element={<Website />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/transactions' element={<Transactions />} />
+        <Route path='/watchlist' element={<Watchlist />} />
+        <Route path='/wallet' element={<Wallet />} />
+        <Route path='/settings' element={<Settings />} />
         <Route path='/home' element={<Home />} />
+
         </Routes>
       
     </Router>
---
+++ WEBSITE/CWMS/src/pages/Transactions.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+function Transactions() {
+  return (
+    <div>
+    {/* <Navbar /> */}
+    <section className="transactions">
+        <h2>Recent Transactions</h2>
+        <h4>transaction1</h4>
+        <h4>transaction2</h4>
+        <h4>transaction3</h4>
+        <h4>transaction4</h4>
+        <h4>transaction5</h4>
+        <br></br>
+        <a href="#" className="view-button">View</a>
+      </section>
+      </div>
+  );
+}
+
+export default Transactions;
+
