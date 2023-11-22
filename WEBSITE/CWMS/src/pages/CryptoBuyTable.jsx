a9b1a5f61c158447f9c97a8acd07f291390a02bc
Jeswin Binu <jeswinbinu2003@gmail.com>
2023-11-23 03:16:38 +0530

    crypto buy table

--- WEBSITE/CWMS/src/pages/CryptoBuyTable.jsx
+++ WEBSITE/CWMS/src/pages/CryptoBuyTable.jsx
@@ -1,31 +1,37 @@
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react';
-import '../styles/crypto.css';
+// import React, { useEffect } from 'react';
+// import '../styles/crypto.css';
+
+// const CryptoBuyTable = () => {
+//   useEffect(() => {
+//     const script = document.createElement('script');
+//     script.src = 'https://www.livecoinwatch.com/static/lcw-widget.js';
+//     script.defer = true;
+//     document.body.appendChild(script);
+//   }, []);
+
+//   return (
+//     <div class="livecoinwatch-widget-container">
+//       <div
+//         class="livecoinwatch-widget-3"
+//         lcw-base="USD"
+//         lcw-d-head="true"
+//         lcw-d-name="true"
+//         lcw-d-code="true"
+//         lcw-d-icon="true"
+//         lcw-color-tx="#000000"
+//         lcw-color-bg="#ffffff"
+//         lcw-border-w="20"
+//         style={{ width: '100%' }}
+//       ></div>    </div>
+//   );
+// };
+
+// export default CryptoBuyTable;
+import CryptoTable from "./CryptoTable";
 
 const CryptoBuyTable = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://www.livecoinwatch.com/static/lcw-widget.js';
-    script.defer = true;
-    document.body.appendChild(script);
-  }, []);
-
-  return (
-    <div class="livecoinwatch-widget-container">
-      <div
-        class="livecoinwatch-widget-3"
-        lcw-base="USD"
-        lcw-d-head="true"
-        lcw-d-name="true"
-        lcw-d-code="true"
-        lcw-d-icon="true"
-        lcw-color-tx="#000000"
-        lcw-color-bg="#ffffff"
-        lcw-border-w="20"
-        style={{ width: '100%' }}
-      ></div>    </div>
-  );
+  return <CryptoTable />;
 };
-
-export default CryptoBuyTable;
+export default CryptoBuyTable
