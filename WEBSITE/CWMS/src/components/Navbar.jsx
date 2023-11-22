cbb0dfd4e42ba1f3f52b3ca1d5b548f00cb9c525
KEVIN JOSEPH HENDRY <kevinjosephhendry@gmail.com>
2023-11-22 22:30:05 +0530

    nav

--- WEBSITE/CWMS/src/components/Navbar.jsx
+++ WEBSITE/CWMS/src/components/Navbar.jsx
@@ -1,28 +1,21 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */     
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = ({ isLoggedIn, handleLogout }) => {
+const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        {isLoggedIn ? (
-          <>
-            <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/searchcrypto">Cryptocurrencies</Link></li>
-            <li><Link to="/buysell">Trade</Link></li>
-            <li><Link to="/transactions">Transactions</Link></li>
-            <li><Link to="/watchlist">Watchlist</Link></li>
-            <li><Link to="/wallet">Wallet</Link></li>
-            <li><button onClick={handleLogout}>Logout</button></li>
-          </>
-        ) : (
-          <li><Link to=" ">Login</Link></li>
-        )}
-      </ul>
-    </nav>
-  );
-};
+          <nav className="navbar">
+            <ul className="nav-links">
+              <li><Link to="/dashboard">Home</Link></li>
+              <li><Link to="/searchcrypto">Cryptocurrencies</Link></li>
+              <li><Link to="/buysell">Trade</Link></li>
+              <li><Link to="/transactions">Transactions</Link></li>
+              <li><Link to="/watchlist">Watchlist</Link></li>
+              <li><Link to="/wallet">Wallet</Link></li>
+            </ul>
+          </nav>
+            );
+}
 
 export default Navbar;
+
