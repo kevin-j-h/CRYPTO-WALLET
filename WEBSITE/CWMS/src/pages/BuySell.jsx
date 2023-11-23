8cc82c0ece832afb7459c28a28c43562a9080929
Alaka A J <alaka03aj@gmail.com>
2023-11-23 12:48:45 +0530

    BuySell

--- WEBSITE/CWMS/src/pages/BuySell.jsx
+++ WEBSITE/CWMS/src/pages/BuySell.jsx
@@ -3,24 +3,25 @@ import React, { useEffect, useState } from "react";
 import { supabase } from "../config/supabaseClient";
 import CryptoBuyTable from "./CryptoBuyTable";
 import { useLocation } from "react-router-dom";
-import { v4 as uuidv4 } from 'uuid';  
+import { v4 as uuidv4 } from 'uuid';
 
 import "../styles/buysell.css";
+
 function CryptoBuySell() {
   const [wallet, setWallet] = useState("");
   const location = useLocation();
   const id = location.state;
-  console.log(id)
   const uid = id.userid;
-  console.log(wallet);
+
   const [action, setAction] = useState("");
   const [crypto, setCrypto] = useState("");
   const [amount, setAmount] = useState("");
   const [cryptoHoldings, setCryptoHoldings] = useState([]);
   const [cryptoBal, setCryptoBal] = useState([]);
+  const [actualCryptoBal, setActualCryptoBal] = useState([]); // Add this line
   const [cryptocurrencies, setCryptocurrencies] = useState([]);
+  const [totalBalance, setTotalBalance] = useState(0);
 
-  // fetches all crypto in crypto table
   useEffect(() => {
     const fetchCryptocurrencies = async () => {
       try {
@@ -37,7 +38,6 @@ function CryptoBuySell() {
     fetchCryptocurrencies();
   }, []);
 
-  // Assuming wallet object has cryptoHoldings and cryptoBal properties
   useEffect(() => {
     const fetchWallet = async () => {
       try {
@@ -52,13 +52,44 @@ function CryptoBuySell() {
         setWallet(data);
         setCryptoHoldings(data.cryptoid || []);
         setCryptoBal(data.quantity || []);
+        setActualCryptoBal(data.quantity || []); // Initialize actualCryptoBal
       } catch (error) {
         console.log(error.message);
       }
     };
 
     fetchWallet();
-  }, [action, amount]); // Dependency array with id
+  }, [action, amount]);
+
+  useEffect(() => {
+    const fetchPricesAndCalculateBalance = async () => {
+      try {
+        const { data: prices, error } = await supabase
+          .from("cryptocurrency")
+          .select()
+          .in("cryptoid", cryptoHoldings);
+
+        if (error) {
+          throw error;
+        }
+
+        const newTotalBalance = prices.reduce((acc, { cryptoid, cryptoprice }) => {
+          const index = cryptoHoldings.indexOf(cryptoid);
+          if (index !== -1) {
+            const balance = cryptoBal[index];
+            return acc + (cryptoprice * balance);
+          }
+          return acc;
+        }, 0);
+
+        setTotalBalance(newTotalBalance);
+      } catch (error) {
+        console.error("Error fetching prices and calculating balance:", error.message);
+      }
+    };
+
+    fetchPricesAndCalculateBalance();
+  }, [cryptoHoldings, cryptoBal, action]);
 
   const handleCryptoChange = (event) => {
     setCrypto(event.target.value);
@@ -74,52 +105,86 @@ function CryptoBuySell() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    handleBuySell();
+  };
+
+  const getCryptoPrice = async (cryptoId) => {
+    try {
+      const { data, error } = await supabase
+        .from("cryptocurrency")
+        .select("cryptoprice")
+        .eq("cryptoid", cryptoId)
+        .single();
+  
+      if (error) {
+        throw error;
+      }
+  
+      return data ? data.cryptoprice : 0; // Return 0 if data is not available
+    } catch (error) {
+      console.error("Error fetching cryptocurrency price:", error.message);
+      return 0; // Return 0 in case of an error
+    }
+  };
+
+  const handleBuySell = async () => {
     if (!crypto || !action || !amount) {
       console.error("Please fill in all details");
       return;
     }
-    console.log(`You want to ${action} ${amount} ${crypto}`);
-
-    if (action === "buy") {
-      // add to wallet, transaction, sub bal
-      // Implement the buy logic here
-    } else if (action === "sell") {
-      // sub from wallet, transaction, add bal
-      // Implement the sell logic here
-      const index = cryptoHoldings.indexOf(crypto);
+  
+    const index = cryptoHoldings.indexOf(crypto);
+  
+    if (action === "sell") {
       if (index !== -1) {
         const bal = cryptoBal[index];
+        console.log(wallet.balance)
         if (bal >= amount) {
           const updatedCryptoBal = [...cryptoBal];
-          updatedCryptoBal[index] = bal - amount;
+          updatedCryptoBal[index] = bal - parseInt(amount);
           setCryptoBal(updatedCryptoBal);
-
+  
+          const updatedActualCryptoBal = [...actualCryptoBal];
+          updatedActualCryptoBal[index] = bal - parseInt(amount);
+          setActualCryptoBal(updatedActualCryptoBal);
+  
           try {
-            const { data, error } = await supabase
+            const cryptoPrice = await getCryptoPrice(crypto);
+            const totalCost = cryptoPrice * parseInt(amount);
+  
+            await supabase
               .from("wallet")
-              .update({ quantity: updatedCryptoBal })
+              .update({
+                quantity: updatedActualCryptoBal,
+                balance: wallet.balance + totalCost,
+              })
               .eq("walletid", wallet.walletid);
-
-            if (error) {
-              console.error(
-                "Error updating cryptoBal in the wallet table:",
-                error.message
-              );
-            } else {
-              console.log("CryptoBal updated in the wallet table:", data);
-              try {
-                const tId = uuidv4();
-                const { data, error } = await supabase.from("transaction").insert({transactionid: tId, timestamp: new Date().toISOString(), amount: amount, walletid: id.walletid, userid: uid});
-                if (error) {throw error;}
-              } catch (error) {
-                console.log(error.message)
-              }
+  
+            const tId = uuidv4();
+            await supabase.from("transaction").insert({
+              transactionid: tId,
+              timestamp: new Date().toISOString(),
+              amount: amount,
+              walletid: id.walletid,
+              userid: uid,
+            });
+  
+            // Fetch updated wallet data after the sell operation
+            const { data: updatedWallet, error: walletError } = await supabase
+              .from("wallet")
+              .select()
+              .eq("walletid", wallet.walletid)
+              .single();
+  
+            if (walletError) {
+              throw walletError;
             }
+  
+            setWallet(updatedWallet);
+  
+            console.log("Sell operation completed successfully!");
           } catch (error) {
-            console.error(
-              "Error updating cryptoBal in the wallet table:",
-              error.message
-            );
+            console.error("Error updating wallet or inserting transaction:", error.message);
           }
         } else {
           console.error("Not enough crypto to sell the given amount!");
@@ -127,46 +192,110 @@ function CryptoBuySell() {
       } else {
         console.error("The mentioned crypto is not in your holdings!");
       }
+    }else if (action === "buy") {
+      const selectedCrypto = cryptocurrencies.find((c) => c.cryptoid === crypto);
+  
+      if (selectedCrypto) {
+        const cryptoPrice = selectedCrypto.cryptoprice;
+        const totalCost = cryptoPrice * parseInt(amount);
+        console.log(wallet.balance)
+  
+        if (wallet.balance >= totalCost) {
+          if (index !== -1) {
+            const bal = cryptoBal[index];
+            const updatedCryptoBal = [...cryptoBal];
+            updatedCryptoBal[index] = bal + parseInt(amount);
+            setCryptoBal(updatedCryptoBal);
+  
+            const updatedActualCryptoBal = [...cryptoBal];
+            updatedActualCryptoBal[index] = bal + parseInt(amount);
+            setTotalBalance(calculateTotalBalance(updatedActualCryptoBal));
+  
+            try {
+              await supabase
+                .from("wallet")
+                .update({
+                  quantity: updatedActualCryptoBal,
+                  balance: wallet.balance - totalCost
+                })
+                .eq("walletid", wallet.walletid);
+  
+              const tId = uuidv4();
+              await supabase.from("transaction").insert({
+                transactionid: tId,
+                timestamp: new Date().toISOString(),
+                amount: amount,
+                walletid: id.walletid,
+                userid: uid
+              });
+  
+              console.log("Buy operation completed successfully!");
+            } catch (error) {
+              console.error("Error updating wallet or inserting transaction:", error.message);
+            }
+          } else {
+            console.error("The mentioned crypto is not in your holdings!");
+          }
+        } else {
+          console.error("Not enough balance to make the purchase!");
+        }
+      } else {
+        console.error("Selected cryptocurrency not found!");
+      }
     }
+  };
+  
+
+  const calculateTotalBalance = (updatedActualCryptoBal) => {
+    const newTotalBalance = cryptocurrencies.reduce((acc, { cryptoid, cryptoprice }) => {
+      const index = cryptoHoldings.indexOf(cryptoid);
+      if (index !== -1) {
+        const balance = updatedActualCryptoBal[index];
+        return acc + (cryptoprice * balance);
+      }
+      return acc;
+    }, 0);
+
+    return newTotalBalance;
   };
 
   return (
     <div className="page1">
-    <section className="buysell">
-      <h1>Buy-Sell Cryptocurrencies</h1>
-      <CryptoBuyTable />
-      <div className="buysell-list">
-        <div className="crypto1">
-          {/* <p>{crypto}</p> */}
-          <form onSubmit={handleSubmit}>
-            <select value={crypto} onChange={handleCryptoChange}>
-              <option value="">Select a cryptocurrency</option>
-              {cryptocurrencies.map((crypto) => (
-                <option key={crypto.symbol} value={crypto.cryptoid}>
-                  {crypto.symbol}
-                </option>
-              ))}
-            </select>
-
-            <input
-              type="text"
-              value={amount}
-              onChange={handleAmountChange}
-              placeholder="Enter the amount"
-            />
-
-            <div className="custom-select">
-              <select value={action} onChange={handleActionChange}>
-                <option value="">Select an action</option>
-                <option value="buy">Buy</option>
-                <option value="sell">Sell</option>
+      <section className="buysell">
+        <h1>Buy-Sell Cryptocurrencies</h1>
+        <CryptoBuyTable />
+        <div className="buysell-list">
+          <div className="crypto1">
+            <form onSubmit={handleSubmit}>
+              <select value={crypto} onChange={handleCryptoChange}>
+                <option value="">Select a cryptocurrency</option>
+                {cryptocurrencies.map((crypto) => (
+                  <option key={crypto.symbol} value={crypto.cryptoid}>
+                    {crypto.symbol}
+                  </option>
+                ))}
               </select>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
+
+              <input
+                type="text"
+                value={amount}
+                onChange={handleAmountChange}
+                placeholder="Enter the amount"
+              />
+
+              <div className="custom-select">
+                <select value={action} onChange={handleActionChange}>
+                  <option value="">Select an action</option>
+                  <option value="buy">Buy</option>
+                  <option value="sell">Sell</option>
+                </select>
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+            {/* <p>Total Balance: {wallet.balance}</p> */}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </div>
   );
 }
--- WEBSITE/CWMS/src/pages/Watchlist.jsx
+++ WEBSITE/CWMS/src/pages/Watchlist.jsx
@@ -1,92 +1,209 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+// import { supabase } from '../config/supabaseClient';
+// import { useLocation, useNavigate } from 'react-router-dom';
+// import "../styles/watchlist.css";
+
+// const Watchlist = () => {
+//   const navigate = useNavigate();
+//   const location = useLocation();
+//   const id = location.state;
+//   const [watchlist, setWatchlist] = useState([]);
+//   const [cryptoInfoArray, setCryptoInfoArray] = useState([]);
+
+//   useEffect(() => {
+//     const fetchWatchlistData = async () => {
+//       try {
+//         const { data: watchlistData, error: watchlistError } = await supabase
+//           .from('watchlist')
+//           .select('cryptoid')
+//           .eq('userid', id);
+
+//         if (watchlistError) {
+//           console.error('Error fetching watchlist data:', watchlistError.message);
+//           return;
+//         }
+
+//         const watch = watchlistData[0]?.cryptoid || [];
+//         setWatchlist(watch);
+//       } catch (error) {
+//         console.error('Error in fetchWatchlistData:', error.message);
+//       }
+//     };
+
+//     fetchWatchlistData();
+//   }, [id]);
+
+//   useEffect(() => {
+//     const fetchDataForWatchlist = async () => {
+//       try {
+//         const promises = watchlist.map(async (watchlistItem) => {
+//           const { data: cryptoData, error: cryptoError } = await supabase
+//             .from('cryptocurrency')
+//             .select()
+//             .eq('cryptoid', watchlistItem);
+
+//           if (cryptoError) {
+//             console.error('Error fetching cryptocurrency data:', cryptoError.message);
+//             return null;
+//           }
+
+//           return cryptoData[0];
+//         });
+
+//         const cryptoInfoArray = await Promise.all(promises);
+//         setCryptoInfoArray(cryptoInfoArray);
+//       } catch (error) {
+//         console.error('Error in fetching cryptocurrency data:', error.message);
+//       }
+//     };
+
+//     fetchDataForWatchlist();
+//   }, [watchlist]);
+
+//   return (
+//     <div className='pagee'>
+      
+//     <div className="watchlist-container">
+//       <h2>My Watchlist</h2>
+//       <button onClick={() => navigate('addWatchlist', {state: id})}>Add</button>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Symbol</th>
+//             <th>Crypto Name</th>
+//             <th>Price</th>
+//             <th>Market Cap</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {cryptoInfoArray.map((cryptoInfo, index) => (
+//             <tr key={index}>
+//               <td>{cryptoInfo.symbol}</td>
+//               <td>{cryptoInfo.cryptoname}</td>
+//               <td>{cryptoInfo.cryptoprice}</td>
+//               <td>{cryptoInfo.marketcap}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//     </div>
+//   );
+// };
+
+// export default Watchlist;
+import React, { useEffect, useState } from 'react';
 import { supabase } from '../config/supabaseClient';
-import { useLocation, useNavigate } from 'react-router-dom';
-import "../styles/watchlist.css";
+import '../styles/watchlist.css';
 
 const Watchlist = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const id = location.state;
+  const [cryptos, setCryptos] = useState([]);
+  const [selectedCrypto, setSelectedCrypto] = useState('');
   const [watchlist, setWatchlist] = useState([]);
-  const [cryptoInfoArray, setCryptoInfoArray] = useState([]);
 
   useEffect(() => {
-    const fetchWatchlistData = async () => {
+    async function fetchCryptoData() {
       try {
-        const { data: watchlistData, error: watchlistError } = await supabase
-          .from('watchlist')
-          .select('cryptoid')
-          .eq('userid', id);
+        let { data: cryptocurrency, error } = await supabase
+          .from('cryptocurrency')
+          .select('cryptoid, cryptoname, cryptoprice, symbol');
 
-        if (watchlistError) {
-          console.error('Error fetching watchlist data:', watchlistError.message);
-          return;
+        if (error) {
+          throw error;
         }
 
-        const watch = watchlistData[0]?.cryptoid || [];
-        setWatchlist(watch);
+        setCryptos(cryptocurrency);
       } catch (error) {
-        console.error('Error in fetchWatchlistData:', error.message);
+        console.error('Error fetching crypto data:', error.message);
       }
-    };
+    }
 
-    fetchWatchlistData();
-  }, [id]);
-
-  useEffect(() => {
-    const fetchDataForWatchlist = async () => {
+    async function fetchWatchlistData() {
       try {
-        const promises = watchlist.map(async (watchlistItem) => {
-          const { data: cryptoData, error: cryptoError } = await supabase
-            .from('cryptocurrency')
-            .select()
-            .eq('cryptoid', watchlistItem);
+        let { data: watchlistData, error } = await supabase
+          .from('watchlist')
+          .select('cryptoid, date')
+          .order('date', { ascending: false });
 
-          if (cryptoError) {
-            console.error('Error fetching cryptocurrency data:', cryptoError.message);
-            return null;
-          }
+        if (error) {
+          throw error;
+        }
 
-          return cryptoData[0];
-        });
-
-        const cryptoInfoArray = await Promise.all(promises);
-        setCryptoInfoArray(cryptoInfoArray);
+        setWatchlist(watchlistData);
       } catch (error) {
-        console.error('Error in fetching cryptocurrency data:', error.message);
+        console.error('Error fetching watchlist data:', error.message);
       }
-    };
+    }
 
-    fetchDataForWatchlist();
-  }, [watchlist]);
+    fetchCryptoData();
+    fetchWatchlistData();
+  }, []);
+
+  const handleAddToWatchlist = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('watchlist')
+        .insert([{ cryptoid: selectedCrypto }]);
+
+      if (error) {
+        throw error;
+      }
+
+      console.log('Crypto added to watchlist:', data);
+      // Refresh watchlist data
+      // This could be optimized by manually updating the state instead of re-fetching
+      const { data: updatedWatchlist, error: updatedError } = await supabase
+        .from('watchlist')
+        .select('cryptoid, date')
+        .order('date', { ascending: false });
+
+      if (updatedError) {
+        throw updatedError;
+      }
+
+      setWatchlist(updatedWatchlist);
+    } catch (error) {
+      console.error('Error adding crypto to watchlist:', error.message);
+    }
+  };
 
   return (
-    <div className='pagee'>
-      
     <div className="watchlist-container">
       <h2>My Watchlist</h2>
-      <button onClick={() => navigate('addWatchlist', {state: id})}>Add</button>
-      <table>
-        <thead>
-          <tr>
-            <th>Symbol</th>
-            <th>Crypto Name</th>
-            <th>Price</th>
-            <th>Market Cap</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cryptoInfoArray.map((cryptoInfo, index) => (
-            <tr key={index}>
-              <td>{cryptoInfo.symbol}</td>
-              <td>{cryptoInfo.cryptoname}</td>
-              <td>{cryptoInfo.cryptoprice}</td>
-              <td>{cryptoInfo.marketcap}</td>
-            </tr>
+      <div className="add-crypto">
+        <select value={selectedCrypto} onChange={(e) => setSelectedCrypto(e.target.value)}>
+          <option value="">Select a cryptocurrency</option>
+          {cryptos.map((crypto) => (
+            <option key={crypto.cryptoid} value={crypto.cryptoid}>
+              {crypto.cryptoname} - {crypto.symbol}
+            </option>
           ))}
-        </tbody>
-      </table>
-    </div>
+        </select>
+        <button onClick={handleAddToWatchlist}>Add to Watchlist</button>
+      </div>
+      <div className="watchlist-items">
+        <h3>Watchlist Items</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Crypto Name</th>
+              <th>Symbol</th>
+              <th>Crypto Price</th>
+              <th>Watchlist Timestamp</th>
+            </tr>
+          </thead>
+          <tbody>
+            {watchlist.map((item, index) => (
+              <tr key={index}>
+                <td>{/* Fetch crypto details based on item.cryptoid */}</td>
+                <td>{/* Fetch crypto details based on item.cryptoid */}</td>
+                <td>{/* Fetch crypto details based on item.cryptoid */}</td>
+                <td>{item.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
